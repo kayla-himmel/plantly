269e16e83eac8e0398b004891dedb1cf25410e84
kayla-himmel-telus <kayla.himmelberger@telus.com>
2026-05-29 11:30:14 -0700

    plantly: added initial styling for button, haptics, background gradient

---
+++ components/PlantlyButton.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, Pressable, Platform } from "react-native";
+import * as Haptics from "expo-haptics";
+import { theme } from "@/theme";
+
+type Props = {
+  title: string;
+  onPress: () => void;
+};
+
+export function PlantlyButton({ title, onPress }: Props) {
+  const handleOnPress = () => {
+    if (Platform.OS !== "web") {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    }
+    onPress();
+  };
+
+  return (
+    <Pressable
+      onPress={handleOnPress}
+      style={(state) => {
+        if (state.pressed) {
+          return [styles.button, styles.buttonPressed];
+        }
+        return styles.button;
+      }}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  button: {
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 6,
+    backgroundColor: theme.colorGreen,
+  },
+  buttonPressed: {
+    backgroundColor: theme.colorDarkGreen,
+  },
+});
--- theme.ts
+++ theme.ts
@@ -1,4 +1,7 @@
 export const theme = {
+  colorDarkGreen: "#206a42",
   colorGreen: "#29b365",
+  colorAppleGreen: "#a0d36c",
+  colorLimeGreen: "#d0e57e",
   colorWhite: "#fff",
 };
